1410ec4dc4131ab1c782a02b7c340ba8a68771c7
Caio Cesar - Fatec <caio.lima20@fatec.sp.gov.br>
2026-04-08 20:32:31 -0300

    feat: varias telas

--- App11.js
+++ App11.js
@@ -4,11 +4,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from './src/screens/HomeScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
+import App2 from './App2'
+import App3 from './App3'
+import App6 from './App6'
+import App7 from './App7'
+import App8 from './App8'
+import App9 from './App9'
+import App10 from './App10'
+
 
 // Criando o Stack Navigator
 const Stack = createStackNavigator();
 
-export default function App() {
+export default function App11() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -16,6 +24,14 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         {/* Definindo a tela "Details" */}
         <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="App2" component={App2} />
+        <Stack.Screen name="App3" component={App3} />
+        <Stack.Screen name="App6" component={App6} />
+        <Stack.Screen name="App7" component={App7} />
+        <Stack.Screen name="App8" component={App8} />
+        <Stack.Screen name="App9" component={App9} />
+        <Stack.Screen name="App10" component={App10} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,12 +3,53 @@ import { View, Text, Button } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        gap: 10 
+        }}>
       <Text>Bem-vindo à Tela Home</Text>
       <Button
         title="Ir para Detalhes"
         onPress={() => navigation.navigate('Details', { itemId: 42 })}
       />
+      <Button
+        title="Ir para App2"
+        onPress={() => navigation.navigate('App2')}
+      />
+      <Button
+        title="Ir para App3"
+        onPress={() => navigation.navigate('App3')}
+      />
+      <Button
+        title="Ir para App4"
+        onPress={() => navigation.navigate('App4')}
+      />
+      <Button
+        title="Ir para App5"
+        onPress={() => navigation.navigate('App5')}
+      />
+      <Button
+        title="Ir para App6"
+        onPress={() => navigation.navigate('App6')}
+      />
+      <Button
+        title="Ir para App7"
+        onPress={() => navigation.navigate('App7')}
+      />
+      <Button
+        title="Ir para App8"
+        onPress={() => navigation.navigate('App8')}
+      />
+      <Button
+        title="Ir para App9"
+        onPress={() => navigation.navigate('App9')}
+      />
+      <Button
+        title="Ir para App10"
+        onPress={() => navigation.navigate('App10')}
+      />
+
     </View>
   );
 }
