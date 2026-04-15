9fb1c738de39cf6e5a84c7a5666cd46882ca317a
Caio Cesar - Fatec <caio.lima20@fatec.sp.gov.br>
2026-04-15 19:30:11 -0300

    feat: App13 e ajustes em cadastro

---
+++ App13.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+
+export default function App13() {
+  return (
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Header App</Text>
+      </View>
+  );
+};
